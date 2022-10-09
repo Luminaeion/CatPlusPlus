8827d4b0211b4f6ceda4b41edbfa7c4a805bb713
Katri <luminaeion@gmail.com>
2022-10-09 23:53:37 +0300

    fixed random encounters :P

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char name[50];
+    cout << "Hello. You are a cat." << endl;
+    cout << "What is your name?" << endl;
+    cin.getline(name, 50);
+    cout << "Welcome, " << name << "." << endl;
+    
     return 0;
 }
