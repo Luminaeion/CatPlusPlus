ad76d6ac1780f848ed96d21320c2d90fa17cc29f
Katri <luminaeion@gmail.com>
2022-10-09 23:11:57 +0300

    Player input formatting works as intended

--- test.cpp
+++ test.cpp
@@ -6,20 +6,28 @@
 #include <fstream>
 #include <bits/stdc++.h>
 
+#include "cat.h"
 #include "playerFunctions.hpp"
 
 using namespace std;
 
 int main()
 {
+
     cout << "\n--------------------- Start of test ---------------------" << endl;
 
     // Start of the game, player input test
     char name[50];
     cout << "Hello. You are a cat." << endl;
+    Cat catProtag;
+    cout << "Cat\n"
+        << "- MaxHP: " << catProtag.getMaxHP() << '\n'
+        << "- Strength: " << catProtag.getStrength() << endl;
     cout << "What is your name?" << endl;
     cin.getline(name, 50);
     cout << "Welcome, " << name << "." << endl;
+
+
 
     // Test function to make player inputs lowercase
     cout << "What colour is your fur?" << endl;
@@ -27,12 +35,11 @@ int main()
     cout << "You are a " << colour << " cat called " << name << "." << endl;
 
     // Player interaction test
-    string tutorialChoice;
     cout << "You encounter a *menacing* ball of yarn." << endl;
     retry:
     cout << "What will you do?" << endl;
     cout << "[ Look ] - [ Paw ] - [ Leave ]" << endl;
-    cin >> tutorialChoice;
+    string tutorialChoice = playerChoice();
     
     if(tutorialChoice == "look")
     {
@@ -50,108 +57,131 @@ int main()
         goto retry;
     }
 
-    string wanderAround;
+    string wanderOpt;
+    int encounterNum = 0;
     repeat:
     cout << "You wander around but come across nothing of interest. Keep wandering?" << endl;
-    cin >> wanderAround;
-    if(wanderAround == "yes"){
+    cin >> wanderOpt;
+    if(wanderOpt == "yes"){
 
-    
-    // Random encounter test
-    int randomEvent;
-    int encounterNum = 0;
-    srand((unsigned)time(0));
-    int rnum = (rand()%10) + 1;
-    int pnum = (rand()%10) + 1;
-    if(rnum == pnum){
-        forceEncounter:
-        switch(rnum){
-            case 1:
-            case 2:
-            {
-                string playerAction;
-                cout << "You find a <food item> that looks particularly delicious. Pick it up?" << endl;
-                cout << "Yes or no" << endl;
-                cin >> playerAction;
-                if(playerAction == "yes"){
-                    cout << "You pick it up." << endl;
-                    cout << "Obtained <food item>!" << endl;
-                } else if(playerAction == "no") {
-                    cout << "You don't like this particular <food item>. You decide to leave it where it is." << endl;
+        // Random encounter test
+        int randomEvent;
+        srand((unsigned)time(0));
+        int rnum = (rand()%10) + 1;
+        int pnum = (rand()%10) + 1;
+        if(rnum == pnum){
+            forceEncounter:
+            switch(rnum){
+                case 1:
+                case 2:
+                {
+                    string playerAction;
+                    cout << "You find a <food item> that looks particularly delicious. Pick it up?" << endl;
+                    cout << "Yes or no" << endl;
+                    cin >> playerAction;
+                    if(playerAction == "yes"){
+                        cout << "You pick it up." << endl;
+                        cout << "Obtained <food item>!" << endl;
+                    } else if(playerAction == "no") {
+                        cout << "You don't like this particular <food item>. You decide to leave it where it is." << endl;
+                    }
                 }
-            }
-            break;
-            case 3:
-            case 4:
-            {
-                string playerAction;
-                cout << "You find an <item>. It might be useful. Pick it up?" << endl;
-                cout << "Yes or no" << endl;
-                cin >> playerAction;
-                if(playerAction == "yes"){
-                    cout << "You pick the <item> up." << endl;
-                    cout << "Obtained <item>!" << endl;
-                } else if(playerAction == "no"){
-                    cout << "You ignore the <item> and walk away." << endl;
+                break;
+                case 3:
+                case 4:
+                {
+                    string playerAction;
+                    cout << "You find an <item>. It might be useful. Pick it up?" << endl;
+                    cout << "Yes or no" << endl;
+                    cin >> playerAction;
+                    if(playerAction == "yes"){
+                        cout << "You pick the <item> up." << endl;
+                        cout << "Obtained <item>!" << endl;
+                    } else if(playerAction == "no"){
+                        cout << "You ignore the <item> and walk away." << endl;
+                    }
                 }
-            }
-            break;
-            case 5:
-            case 6:
-            {
-                string playerAction;
-                cout << "An <enemy> appears! They haven't noticed you yet." << endl;
-                cout << "What will you do?" << endl;
-                cin >> playerAction;
-                if(playerAction == "run"){
-                    cout << "You flee before you're noticed." << endl;
-                    cout << "Escaped safely." << endl;
-                } else if (playerAction == "fight"){
-                    cout << "You take the <enemy> by surprise!" << endl;
+                break;
+                case 5:
+                case 6:
+                {
+                    string playerAction;
+                    cout << "An <enemy> appears! They haven't noticed you yet." << endl;
+                    cout << "What will you do?" << endl;
+                    cin >> playerAction;
+                    if(playerAction == "run"){
+                        cout << "You flee before you're noticed." << endl;
+                        cout << "Escaped safely." << endl;
+                    } else if (playerAction == "fight"){
+                        cout << "You take the <enemy> by surprise!" << endl;
+                    }
                 }
-            }
-            break;
-            case 7:
-            case 8:
-            {
-                string playerAction;
-                cout << "You find an <item>. It might be useful. Pick it up?" << endl;
-                cout << "Yes or no" << endl;
-                cin >> playerAction;
-                if(playerAction == "yes"){
-                    cout << "You pick the <item> up." << endl;
-                    cout << "Obtained <item>!" << endl;
-                } else if(playerAction == "no"){
-                    cout << "You decide to leave the <item> where it is." << endl;
+                break;
+                case 7:
+                case 8:
+                {
+                    string playerAction;
+                    cout << "You find an <item>. It might be useful. Pick it up?" << endl;
+                    cout << "Yes or no" << endl;
+                    cin >> playerAction;
+                    if(playerAction == "yes"){
+                        cout << "You pick the <item> up." << endl;
+                        cout << "Obtained <item>!" << endl;
+                    } else if(playerAction == "no"){
+                        cout << "You decide to leave the <item> where it is." << endl;
+                    }
                 }
-            }
-            break;
-            case 9:
-            case 10:
-            {
-                string playerAction;
-                cout << "You find an <item>. It might be useful. Pick it up?" << endl;
-                cout << "Yes or no" << endl;
-                cin >> playerAction;
-                if(playerAction == "yes"){
-                    cout << "You pick the <item> up." << endl;
-                    cout << "Obtained <item>!" << endl;
-                } else if(playerAction == "no"){
-                    cout << "You decide to leave the <item> where it is." << endl;
+                break;
+                case 9:
+                case 10:
+                {
+                    string playerAction;
+                    cout << "You find an <item>. It might be useful. Pick it up?" << endl;
+                    cout << "Yes or no" << endl;
+                    cin >> playerAction;
+                    if(playerAction == "yes"){
+                        cout << "You pick the <item> up." << endl;
+                        cout << "Obtained <item>!" << endl;
+                    } else if(playerAction == "no"){
+                        cout << "You decide to leave the <item> where it is." << endl;
+                    }
                 }
+                break;
             }
-            break;
+        } else {
+            if(encounterNum >= 4){
+                rnum == pnum;
+                goto forceEncounter;
+            } else {
+                encounterNum++;
+            }
+            goto repeat;
         }
-    } else {
-        if(encounterNum < 4){
-            encounterNum++;
-        } else if(encounterNum >= 4) {
-            rnum == pnum;
-            goto forceEncounter;
+    }/*  else if(wanderOpt == "openinventory"){
+        cout << "You open your inventory." << endl;
+        cout << "[ Stats ] - [ Items ] - [ Save ]" << endl;
+        string menuOpt = playerChoice();
+        
+        if(menuOpt == "stats"){
+            // display playerStats
+        } else if(menuOpt == "items"){
+            // display playerItems
+        } else if(menuOpt == "save"){
+            cout << "[ Save ] - [ Load ] - [ Exit ] - [ Back ]" << endl;
+            string gameOpt = playerChoice();
+            if(gameOpt == "save"){
+                // saveGame(name);
+            } else if(gameOpt == "load"){
+                cout << "Savefiles found: " << /* display saves  << ". Type name of the file you want to load." << endl;
+                // player input for which file should be loaded
+                // loadGame(<selected save user input>)
+            }
+        } else if(menuOpt == "exit"){
+            //exitGame();
+        } else if(menuOpt == "back"){
+            //goto repeat;
         }
-        goto repeat;
-    }
-    }
+    } */
 
     cout << "\n--------------------- End of test ---------------------" << endl;
     _getch();
