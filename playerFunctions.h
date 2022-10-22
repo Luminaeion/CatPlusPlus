0f59452948f2cfaa432a362732ed9cb6227e14aa
Katri <luminaeion@gmail.com>
2022-10-22 22:30:15 +0300

    can accept longer inputs (>=1 words) when needed

--- playerFunctions.h
+++ playerFunctions.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,4 +14,30 @@ string playerChoice() {
     /*transform(playerInput.begin(), playerInput.end(), playerInput.begin(), ::tolower);
     playerInput.erase(remove(playerInput.begin(), playerInput.end(), ' '), playerInput.end());*/
     return playerInput;
+}
+
+
+// basically the same function but this one removes only EXCESS whitespace
+void remove_extra_whitespaces(const string &input, string &output)
+{
+    output.clear();
+    unique_copy (input.begin(), input.end(), back_insert_iterator<string>(output),
+    [](char a,char b){ return isspace(a) && isspace(b);});  
+}
+
+// function to make player inputs lowercase to match what the program expects
+string longInput() {
+    string playerInput;
+    string formattedInput;
+
+    // get user input
+    getline(cin, playerInput);
+
+    // make input lowercase
+    for(auto& c: playerInput) c = tolower(c,cout.getloc());
+
+    // remove excess whitespace
+    remove_extra_whitespaces(playerInput, formattedInput);
+
+    return formattedInput;
 }
