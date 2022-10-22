2e147f136f6e7d8e1fbf115705c5c351e2d77474
Katri <luminaeion@gmail.com>
2022-10-22 22:16:55 +0300

    Better user input formatting (lowercase&no spaces)

--- playerFunctions.h
+++ playerFunctions.h
@@ -7,8 +7,10 @@ using namespace std;
 // function to make player inputs lowercase to match what the program expects
 string playerChoice() {
     string playerInput;
-    cin >> playerInput;
-    transform(playerInput.begin(), playerInput.end(), playerInput.begin(), ::tolower);
+    getline(cin, playerInput);
     playerInput.erase(remove(playerInput.begin(), playerInput.end(), ' '), playerInput.end());
+    for(auto& c: playerInput) c = tolower(c,cout.getloc());
+    /*transform(playerInput.begin(), playerInput.end(), playerInput.begin(), ::tolower);
+    playerInput.erase(remove(playerInput.begin(), playerInput.end(), ' '), playerInput.end());*/
     return playerInput;
 }
